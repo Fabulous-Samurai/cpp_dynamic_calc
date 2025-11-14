bf3f0538f054dcfbf8c65d555eb5c028b73fd44c
Efe Sevil <152120231067@ogrenci.ogu.edu.tr>
2025-11-14 18:57:36 +0300

    Initial commit

---
+++ dynamic_calc.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include "dynamic_calc.h"
+#include <stack>
+#include <stdexcept>
+#include <sstream>
+
+OperationResult Dynamic_calc::EvaluateRPN(std::queue<std::string> &rpn_queue) {
+    std::stack<double> value_stack;
+    while (!rpn_queue.empty()) {
+        std::string token = rpn_queue.front();
+        rpn_queue.pop();
+        if (isdigit(token[0]) || token[0] == '-' && token.size() > 1 && isdigit(token[1])) {
+            try {
+                value_stack.push(std::stod(token));
+            } catch (std::exception &e) {
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+        } else if (ops_.count(token[0])) {
+            if (value_stack.size() < 2) {
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            double val2 = value_stack.top();
+            value_stack.pop();
+            double val1 = value_stack.top();
+            value_stack.pop();
+
+            std::vector<double> args = {val1, val2};
+            OperationResult op_res = ops_.at(token[0]).operation(args);
+            if (op_res.err != CalcErr::None) {
+                return op_res;
+            }
+            value_stack.push(op_res.result.value());
+        } else if (unary_ops.count(token[0])) {
+            if (value_stack.size() < 1) {
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            double val = value_stack.top();
+            value_stack.pop();
+            std::vector<double> args = {val};
+            OperationResult unary_op_res = unary_ops.at(token[0]).unary_operation(args);
+            if (unary_op_res.err != CalcErr::None) {
+                return unary_op_res;
+            }
+            value_stack.push(std::stod(token));
+        }
+        if (value_stack.size() != 1) {
+            return {std::nullopt, CalcErr::ArgumentMismatch};
+        }
+
+    }
+    return {std::optional<double>(value_stack.top()), CalcErr::None};
+};
+
+bool Dynamic_calc::isNumber(const std::string &token) const {
+    if (token.empty()) return false;
+
+    char *end = nullptr;
+    std::strtod(token.c_str(), &end);
+    return (end == (token.c_str() + token.size()));
+};
+
+Precedence Dynamic_calc::get_precedence(const std::string &token) const {
+    if (token.length() != 1) return Precedence::None;
+    char op = token[0];
+    if (ops_.count(op)) {
+        return ops_.at(op).precedence;
+    }
+    if (unary_ops.count(op)) {
+        return unary_ops.at(op).precedence;
+    }
+    return Precedence::None;
+};
+
+bool Dynamic_calc::isLeftAssociative(const std::string &token) const {
+    if (token == "^") {
+        return false;
+    }
+    return true;
+};
+
+std::queue<std::string> Dynamic_calc::ParseToRPN(const std::string &expression) {
+    std::queue<std::string> output_queue;
+    std::stack<std::string> operator_stack;
+
+    std::stringstream ss(expression);
+    std::string token;
+
+    while (ss >> token) {
+
+        if (isNumber(token)) {
+            output_queue.push(token);
+        } else if (token.length() == 1 && unary_ops.count(token[0])) {
+            operator_stack.push(token);
+        } else if (token.length() == 1 && ops_.count(token[0])) {
+            Precedence current_precedence = ops_.at(token[0]).precedence;
+
+
+            while (!operator_stack.empty() &&
+                   operator_stack.top() != "(" &&
+                   (get_precedence(operator_stack.top()) > current_precedence ||
+                    (get_precedence(operator_stack.top()) == current_precedence && isLeftAssociative(token)))
+                    ) {
+
+                output_queue.push(operator_stack.top());
+                operator_stack.pop();
+            }
+
+            operator_stack.push(token);
+        } else if (token == "(") {
+            operator_stack.push(token);
+        } else if (token == ")") {
+
+            while (!operator_stack.empty() && operator_stack.top() != "(") {
+                output_queue.push(operator_stack.top());
+                operator_stack.pop();
+            }
+
+
+            if (!operator_stack.empty()) {
+                operator_stack.pop();
+            }
+        }
+    }
+
+
+    while (!operator_stack.empty()) {
+
+        output_queue.push(operator_stack.top());
+        operator_stack.pop();
+    }
+
+    return output_queue;
+};
+
+int main() {
+    std::string expression;
+    Dynamic_calc calc_;
+    std::cout << "Obtain an Expression\n";
+    std::getline(std::cin, expression);
+    auto evaluate_result = calc_.Evaluate(expression);
+    if(evaluate_result.err == CalcErr::None){
+        std::cout<<"Result :"<<evaluate_result.result.value()<<"\n";
+    }
+    std::cerr << "Hata:";
+    switch (evaluate_result.err) {
+        case CalcErr::DivideByZero :
+            std::cerr << "Can't Dividable by 0!";
+            break;
+        case CalcErr::IndeterminateResult :
+            std::cerr << "IndeterminateResult !";
+            break;
+        case CalcErr::OperationNotFound :
+            std::cerr << "Can't find the Obtained Operation";
+        default:
+            std::cerr << "Unknown Error Occurred!";
+            break;
+    }
+    return 0;
+};
---
+++ dynamic_calc.h
@@ -0,0 +1,178 @@
+//
+// Created by Efe on 14.11.2025.
+//
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <memory>
+#include <functional>
+#include <optional>
+#include <mutex>
+#include <shared_mutex>
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+
+
+#ifndef CPP_DYNAMIC_CALC_DYNAMIC_CALC_H
+#define CPP_DYNAMIC_CALC_DYNAMIC_CALC_H
+
+
+enum class CalcErr {
+    None,
+    DivideByZero,
+    IndeterminateResult,
+    OperationNotFound,
+    ArgumentMismatch,
+    NegativeRoot,
+    LeftAssociative
+};
+
+struct OperationResult {
+    std::optional<double> result = std::nullopt;
+    CalcErr err = CalcErr::None;
+
+    OperationResult() = default;
+
+    OperationResult(std::optional<double> const &res, CalcErr const &error)
+            : result(res), err(error) {}
+};
+
+enum class Precedence : int {
+    None = 0, AddSub = 1, MultiDiv = 2, Pow = 3, Mod = 2, Unary = 4
+};
+
+using Operation = std::function<OperationResult(const std::vector<double> &args)>;
+using UnaryOperation = std::function<OperationResult(const std::vector<double> &args)>;
+struct OperatorDetails {
+    Operation operation;
+    Precedence precedence;
+};
+struct UnaryOperatorDetails {
+    UnaryOperation operation;
+    Precedence precedence = Precedence::Unary;
+};
+
+class Dynamic_calc {
+private:
+    std::map<char, OperatorDetails> ops_;
+    std::map<char, UnaryOperatorDetails> unary_ops_;
+    std::shared_mutex mutex_s;
+
+    bool isNumber(const std::string &token) const;
+
+    Precedence get_precedence(const std::string &token) const;
+
+    bool isLeftAssociative(const std::string &token) const;
+
+    std::queue<std::string> ParseToRPN(const std::string &expression);
+
+    OperationResult EvaluateRPN(std::queue<std::string> &rpn_queue);
+
+public:
+    Dynamic_calc() {
+        std::lock_guard<std::shared_mutex> lock(mutex_s);
+        ops_['+'] = {[](const std::vector<double> &args) -> OperationResult {
+            if (args.size() != 2) {
+                std::cerr << "UB for definition of essential algebraic operator!";
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            return OperationResult{std::optional<double>(args[0] + args[1]), CalcErr::None};
+
+        }, Precedence::AddSub
+        };
+        ops_['-'] = {[](const std::vector<double> &args) -> OperationResult {
+            if (args.size() != 2) {
+                std::cerr << "UB acc to definition of essential algebraic operator!";
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            return OperationResult{std::optional<double>(args[0] - args[1]), CalcErr::None};
+
+        }, Precedence::AddSub
+        };
+        ops_['*'] = {[](const std::vector<double> &args) -> OperationResult {
+            if (args.size() != 2) {
+                std::cerr << "UB acc to definition of essential algebraic operator!";
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            return OperationResult{std::optional<double>(args[0] * args[1]), CalcErr::None};
+
+        }, Precedence::MultiDiv
+        };
+        ops_['/'] = {[](const std::vector<double> &args) -> OperationResult {
+            if (args.size() != 2) {
+                std::cerr << "UB acc to definition of essential algebraic operator!";
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            if (args[1] == 0.0) {
+                if (args[0] == 0.0) {
+                    return OperationResult{std::nullopt, CalcErr::IndeterminateResult};
+                }
+                return OperationResult{std::nullopt, CalcErr::DivideByZero};
+            }
+
+            return OperationResult{std::optional<double>(args[0] / args[1]), CalcErr::None};
+
+        }, Precedence::MultiDiv};
+        ops_['^'] = {[](const std::vector<double> &args) -> OperationResult {
+            if (args.size() != 2) {
+                std::cerr << "UB acc to definition of essential algebraic operator!";
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            if (args[0] == 0.0 && args[1] == 0.0) {
+                return OperationResult{std::nullopt, CalcErr::IndeterminateResult};
+            }
+
+            if (args[0] == 0.0 && args[1] < 0.0) {
+                return OperationResult{std::nullopt, CalcErr::DivideByZero};
+            }
+
+            return OperationResult{std::optional<double>(std::pow(args[0], args[1])), CalcErr::None};
+        }, Precedence::Pow
+        };
+        ops_['%'] = {[](const std::vector<double> &args) -> OperationResult {
+            if (args.size() != 2) {
+                std::cerr << "UB acc to definition of essential algebraic operator!";
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            if (args[1] == 0.0) {
+                if (args[0] == 0.0) { // 0 mod 0
+                    return OperationResult{std::nullopt, CalcErr::IndeterminateResult};
+                }
+                return OperationResult{std::nullopt, CalcErr::DivideByZero}; // N mod 0
+            }
+
+            return OperationResult{std::optional<double>(std::fmod(args[0], args[1])), CalcErr::None};
+        },   Precedence::Mod
+        };
+        unary_ops_['s'] = {[](const std::vector<double> &args) -> OperationResult {
+            if (args.size() != 1) {
+                return {std::nullopt, CalcErr::ArgumentMismatch};
+            }
+            double x = args[0];
+            if (x < 0.0) {
+                return {std::nullopt, CalcErr::NegativeRoot};
+            }
+            return {std::optional<double>(std::sqrt(x)), CalcErr::None};
+        }};
+    }
+
+    OperationResult Evaluate(const std::string &expression) {
+        std::queue<std::string> rpn = ParseToRPN(expression);
+
+        return EvaluateRPN(rpn);
+    };
+
+    Dynamic_calc(const Dynamic_calc &other) = delete;
+
+    Dynamic_calc(Dynamic_calc &&other) noexcept = delete;
+
+    ~Dynamic_calc() noexcept = default;
+};
+
+#endif //CPP_DYNAMIC_CALC_DYNAMIC_CALC_H
